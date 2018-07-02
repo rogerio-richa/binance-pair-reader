15183dda67109ae65c340f24a54f9cd598ef5d05
Rogerio Richa <rogerio.richa@cellcrypt.com>
2018-07-01 23:50:14 -0300

    Added visualization using websockets

--- backoffice.js
+++ backoffice.js
@@ -9,15 +9,12 @@ const chatId = "-284111032";
 const bot = new TelegramBot(token, {polling: true});
 
 // Plotter stuff
-const numberOfSeconds = 60*5; // 5 min
-const timerExpiration = 60; // 1 min
+const numberOfSeconds = 60*60*24*2; // 2 days
+const timerExpiration = 60*5; // 5 min
+const thresholdAlarm = 5 // %
 
-var chartLabel = [0];
-var chartData = [];
 var values = [];
 var timer = []
-
-var currentLabel = 0;
 
 // Binance stuff
 const interval = 1000; //ms
@@ -37,71 +34,181 @@ setInterval (function() {
             return console.dir(error);
         }
 
-        var result =  JSON.parse(body);
+        try { var result =  JSON.parse(body); }
+        catch (e) { console.log("deu pau");}
+
+        var now = new Date();
 
         if (values.length == 0) {
 
             for (var i=0; i < result.length; i++) {
 
                 values.push({symbol: result[i].symbol,
-                             price: [result[i].price]});
+                             price: [result[i].price],
+                             dates: [now.toISOString()]});
                 
                 timer.push(0);
-
-                //console.log(Values[i].symbol);
             }
 
             return;
         }
 
         for (var i=0; i < result.length; i++) {
-            
-            //assert(Values[i].symbol == result[i].symbol, "What");
 
-            if (values[i].price.length > numberOfSeconds) {
+            var pair = result[i].symbol;
+            var index;
 
-                values[i].price.shift();
+            for (var j=0; j < values.length; j++) {
+
+                if (values[j].symbol == pair) {
+                    
+                    index = j;
+                }
             }
 
-            values[i].price.push(result[i].price);
+            if (values[index].price.length > numberOfSeconds) {
 
-            timer[i]--;
+                values[index].price.shift();
+                values[index].dates.shift();
+            }
+
+            values[index].price.push(result[i].price);
+            values[index].dates.push(now);
+
+            timer[index]--;
         }
 
-        if (chartLabel.length > numberOfSeconds) {
+        if (values.length != result.length) {
 
-            chartLabel.shift();
+            console.log("New pairs seem to have been added. Please restart server!");
         }
 
-        chartLabel.push(++currentLabel); // Replace with dates
-        chartData = values[0].price;
-
-        for (var i=0; i < result.length; i++) {
+        for (var i=0; i < values.length; i++) {
             
             var diff = 100*(values[i].price[0] - values[i].price[values[i].price.length-1])/values[i].price[0];
-            //console.log(100*diff);
 
-            if ( Math.abs(diff) > 5 && timer[i] <= 0) {
+            if ( Math.abs(diff) > thresholdAlarm && timer[i] <= 0) {
 
-                console.log(Date());
-                bot.sendMessage(chatId, values[i].symbol + " just varied " + diff + " %");
+                var msg = values[i].symbol + " just varied " + diff + " %" + " -- https://bot.cellcrypt.com/" + values[i].symbol;
+                
+                console.log(msg);
+                bot.sendMessage(chatId, msg);
                 timer[i] = timerExpiration;
+            }
+        }
+
+        // Feed data to browsers listening 
+        for (var i=0; i < cli.length; i++) {
+
+            if (cli[i].pair != "") {
+
+                for (var j=0; j < values.length; j++) {
+
+                    if (cli[i].pair == "\""+values[j].symbol+"\"") {
+                        
+                        var lastItem = values[j].price.length - 1;                        
+                        sendData(cli[i].connection, values[j].price[lastItem], values[j].dates[lastItem]);
+                    }
+                }
             }
         }
     });
     },
 interval);
 
+
 http.createServer(function (req, response) {
     fs.readFile('index.html', 'utf-8', function (err, data) {
+
+        inputURL = req.url.slice(1);
+
+        if (inputURL == "") {
+
+            inputURL = "BTCUSDT";
+        }
+
         response.writeHead(200, { 'Content-Type': 'text/html' });
-  
-        var htmlResult1 = data.replace('[__x__]', JSON.stringify(chartLabel));
-        var result = htmlResult1.replace('[__y__]', JSON.stringify(chartData));
-        
+        var result = data.replace('[symbol]', inputURL);
         response.write(result);
         response.end();
     });
 }).listen(1337, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:1337/');
+
+
+var WebSocket = require('ws');
+
+var cli = [];
+const wss = new WebSocket.Server({ port: 8081 });
+
+wss.on('connection', function connection(ws,req) {
+
+    wss.clients.forEach(function each(client) {
+
+        var index = -1; 
+
+        for (var i=0; i < cli.length; i++) {
+    
+            if (cli[i].connection == client) {
+                
+                index = i;
+            }
+        }
+
+        if (cli.length == 0 || index < 0) {
+
+            cli.push({connection: client, pair: ""});
+            console.log("connected clients: "+ cli.length);
+        }
+    });
+
+    ws.on('message', function incoming(data) {
+
+        for (var i=0; i < cli.length; i++) {
+
+            if (cli[i].connection == ws) {
+
+                cli[i].pair = data;
+
+                // send bulk data
+                for (var j=0; j < values.length; j++) {
+
+                    if (cli[i].pair == "\""+values[j].symbol+"\"") {
+                        
+                        sendBulkData(cli[i].connection, values[j].price, values[j].dates);
+                    }
+                }                
+            }
+
+            console.log(cli[i].pair);
+        }
+    });
+
+    ws.on('close', function close() {
+
+        for (var i=0; i < cli.length; i++) {
+
+            if (cli[i].connection.readyState != WebSocket.OPEN) {
+
+                cli.splice(i, 1);
+            }
+        }
+    });
+});
+
+
+function sendData(client, val, date) {
+    
+    data = [{ "date": date, "value": val }];
+    client.send(JSON.stringify(data));
+}
+
+function sendBulkData(client, values, dates) {
+    
+    var data = [];
+    for (var i=0; i < dates.length; i++) {
+
+        data.push({ "date": dates[i], "value": values[i] });
+    }
+    client.send(JSON.stringify(data));
+}
